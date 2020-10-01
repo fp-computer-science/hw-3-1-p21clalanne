95d35ce89f8f51c1049b364c1c403cd2d0d8c949
christian lalanne <p21clalanne@fairfieldprep.org>
2020-10-01 18:18:37 -0400

    Hw 3-1 is done

---
+++ hw_3-1.py
@@ -0,0 +1,13 @@
+# Author CL 9/29/2020
+
+solution1 = (3 * 5) / (2 + 3) 
+print (solution1) 
+
+solution2 = ((7 + 9) ** (1/2)) * 2
+print (solution2)
+ 
+solution3 = (4 - 7) **3
+print (solution3)
+
+solution4 = .5** (-19 + 100)
+print (solution4)
---
+++ hw_3-1_pt2.py
@@ -0,0 +1,13 @@
+# Author CL 10/1/2020
+
+a = 1
+
+b = 3
+
+c = -4 
+
+first_solution =  (-b + .5**(b ** 2 - 4 * a * c)) / 2 * a
+
+second_solution = (-b - .5**(b ** 2 - 4 * a *c)) / 2 * a
+
+print( first_solution, second_solution)
